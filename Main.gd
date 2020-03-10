7cae540da2ee71dbccde4019c4185420ffb3361b
Butch Wesley <butch.wesley@gmail.com>
2020-03-10 16:11:39 -0400

    don't need to set position

--- Main.gd
+++ Main.gd
@@ -7,7 +7,6 @@ func _add_nodes(how_many):
 		var fs  = FontScene.instance()
 		fs.get_node("WithFont").get_font("font").size = i
 		add_child(fs)
-		fs.set_position(Vector2(i, i))
 
 func _ready():
 	_add_nodes(50)
